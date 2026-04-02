d59606f73ad73354dff2145b06efcc22d91b934a
Harmeet Singh Bhamra <harmeet.bhamra@gmail.com>
2026-04-02 20:26:33 +0000

    Finished changes as per tutorial.

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Harmeet Bhamra",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  email: "harmeet@example.com",
+  gitHub: "harmeetbhamra",
+  instagram: "harmeetbhamra",
+  linkedIn: "harmeetbhamra",
+  medium: "harmeetbhamra",
+  twitter: "harmeetbhamra",
+  youTube: "harmeetbhamra",
 };
 
 const primaryColor = "#4E567E";
